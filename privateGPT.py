0b5df192eea7d1657cf51ec6c76ec4e074f8609d
milescattini <milescattini@gmail.com>
2023-05-17 11:48:35 +1000

    Add fix for clang install of non m1 mac

--- privateGPT.py
+++ privateGPT.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import LlamaCppEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
@@ -8,7 +8,7 @@ import os
 
 load_dotenv()
 
-embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
+llama_embeddings_model = os.environ.get("LLAMA_EMBEDDINGS_MODEL")
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
 
 model_type = os.environ.get('MODEL_TYPE')
@@ -18,8 +18,8 @@ model_n_ctx = os.environ.get('MODEL_N_CTX')
 from constants import CHROMA_SETTINGS
 
 def main():
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+    llama = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)
+    db = Chroma(persist_directory=persist_directory, embedding_function=llama, client_settings=CHROMA_SETTINGS)
     retriever = db.as_retriever()
     # Prepare the LLM
     callbacks = [StreamingStdOutCallbackHandler()]
